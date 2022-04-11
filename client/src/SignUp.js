b602957c6f42bc8251aa84aaac7cf62942b94d6c
Amelia Risner <amelia.risner0@gmail.com>
2022-04-11 16:58:30 -0700

    welcome component built, signup component started

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+      <TextField
+      label="Name"
+      defaultValue=""/>
+      <TextField
+      label="Username"
+      defaultValue=""/>
+      <TextField
+      label="Password"
+      defaultValue=""/>
+    </div>
   )
 }
 
--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Route, Switch } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <div>
+        <h1>Adventures of Nora the Neurotic Panda</h1>
+        <Button>Begin Adventure</Button>
+        <Button>Resume Adventure</Button>
+    </div>
   )
 }
 
