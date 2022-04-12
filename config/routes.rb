d02e39551c17a846b026a91b2c64c9da164064f9
Amelia Risner <amelia.risner0@gmail.com>
2022-04-12 16:40:16 -0700

    the auth nightmare has begun

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :choices, only: [:index, :show]
   resources :situations, only: [:index, :show]
   resources :users
+  post "/login", to: "sessions#create"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
