05c1efc9cc14a9b03ce8937d1991a40c471a91e1
Amelia Risner <amelia.risner0@gmail.com>
2022-04-20 21:54:08 -0700

    path to epilogue 4bbb created

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,8 +34,29 @@ situation2b = Situation.create(story_text: "Nora turned to investigate, walking 
     ")
 situation3bb = Situation.create(story_text: "The man gave a nervous laugh and wrung his hands, but made no motion to leave.
 
-    Nora slid her cell phone off the countertop next to her in a manner she hoped was surreptitious; unfortunately, the man spotted this immediately.
+    Nora slid her cell phone off the countertop next to her in a manner she hoped was surreptitious; unfortunately, the man spotted this immediately.  They made eye contact for a brief still moment before two things happened at once: Nora bolted towards her living room, and the intruder appeared on the other side of her kitchen with her phone in hand.
+    
+    …What?
+    
+    Nora stood in the doorway, dumbfounded.  It was impossible for him to have moved that fast, all her eyes had registered was a yuppy blur.  True fear began to rise in the pit of her stomach, and she turned towards the front door – only to run smack into a small, oddly sturdy woman with dark curly hair.
+    
+    “Forget her, grab the cat!”  The man yelled from the kitchen.
+    
+    Chaos broke out as a third pale intruder burst from her hallway closet, and a fourth from her bathroom.
     ")
+situation4bb = Situation.create(story_text: "Before she can reach it, the front door swings open.  A fifth unknown pale figure appears in the doorframe, taller and more commanding than the rest.
+
+    “EVERYONE CALM DOWN.”  The new figure booms.  A moment of silence sweeps across the room.  The newcomer turns to a petrified Nora.
+    
+    “Nora, I’m sorry for the stress and inconvenience we have caused.  Allow me to introduce myself and explain.  I am Drax, and these are my children.”
+    ")
+situation5bbb = Situation.create(story_text: "Nora arrives at the police station doors gasping for breath.  She has barely a moment to regain her composure before two officers approach her, clearly worried about the visibly distressed panda.  After giving a cursory account of the events from just a few minutes prior, the officers accompany her back to her apartment.
+
+    When Nora re-enters her apartment, it’s as clean and empty as it had been before the morning’s events.
+    
+    The towels in the hallway closet had been refolded.  The spilled salt was cleaned off the table, the shakers back in their positions, the cushions placed back onto the sofa.  Everything was in its place, and there was no evidence of the pale strangers.
+    ")
+situation4bbb_epilogue = Situation.create(story_text: "Nora’s life returns to normal.  Archduke Ferdinand mysteriously returns after a few days.  When Nora gets a cold and stays home from work, she notices his newfound tendency to disappear during business hours; he always comes back, though, so it’s not cause for concern.  The salad establishment Sweetgreen disappears and no one else seems to notice [but no one cares] and Beyond Meat begins to carry duck blood for their Polish customers.  None of this affects Nora, and she continues about her calm, content life.")
 
 puts "seeding choices"
 choice0 = Choice.create(choice_text: "In the Beginning")
@@ -45,6 +66,12 @@ choice2aa = Choice.create(choice_text: "2AA")
 choice2ab = Choice.create(choice_text: "2AB")
 choice2ba = Choice.create(choice_text: "Nora screamed and threw the nearby fruit bowl at his head.")
 choice2bb = Choice.create(choice_text: "“Hello.  I believe you were just leaving.”  Nora said sternly.")
+choice3baa = Choice.create(choice_text: "Nora ran towards the front door.")
+choice3bab = Choice.create(choice_text: "Nora ran to the bedroom for Archduke Ferdinand.")
+choice4bba = Choice.create(choice_text: "Nora takes a steadying breath and allows him to continue.")
+choice4bbb = Choice.create(choice_text: "Nora sprints past the man and books it down the street towards the police station.")
+choice5bbba = Choice.create(choice_text: "Nora insists to the officers that the strangers had just been here, that they had to believe her and search for evidence.")
+choice5bbbb = Choice.create(choice_text: "Nora mumbles an apology and thanks the officers for their time.")
 
 puts "seeding situation_choices..."
 SituationChoice.create(choice: choice0, situation: situation1, outcome: false)
@@ -57,6 +84,16 @@ SituationChoice.create(choice: choice2ab, situation: situation2a, outcome: true)
 SituationChoice.create(choice: choice2ba, situation: situation2b, outcome: true)
 SituationChoice.create(choice: choice2bb, situation: situation2b, outcome: true)
 SituationChoice.create(choice: choice2bb, situation: situation3bb, outcome: false)
+SituationChoice.create(choice: choice3baa, situation: situation3bb, outcome: true)
+SituationChoice.create(choice: choice3bab, situation: situation3bb, outcome: true)
+SituationChoice.create(choice: choice3baa, situation: situation4bb, outcome: false)
+# SituationChoice.create(choice: choice3bab, situation: situation4bb, outcome: false)
+SituationChoice.create(choice: choice4bba, situation: situation4bb, outcome: true)
+SituationChoice.create(choice: choice4bbb, situation: situation4bb, outcome: true)
+SituationChoice.create(choice: choice4bbb, situation: situation5bbb, outcome: false)
+SituationChoice.create(choice: choice5bbba, situation: situation5bbb, outcome: true)
+SituationChoice.create(choice: choice5bbbb, situation: situation5bbb, outcome: true)
+SituationChoice.create(choice: choice5bbbb, situation: situation4bbb_epilogue, outcome: false)
 
 puts "seeding user_choices..."
 user_choice_new = UserChoice.create(user: user2, choice: choice0)
